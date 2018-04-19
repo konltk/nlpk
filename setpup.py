78da4d3e5d45195ef62f241f3813bfda4f5a0ee4
konltk <32383278+konltk@users.noreply.github.com>
2018-04-19 12:05:37 +0900

    Create setpup.py

---
+++ setpup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'nlpk',
+  packages = ['nlpk'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'nlpk',
+  author = 'hyunyoung2',
+  author_email = 'konltk@nvae.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
+
